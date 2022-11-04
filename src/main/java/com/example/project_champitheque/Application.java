3f06dd9659b6de90fed7c26a0de2edd7c5a2c38b
Mathieu <Mathieu.sellier@student.junia.com>
2022-11-04 17:43:08 +0100

    ajout d'une deuxième scene +

---
+++ src/main/java/com/example/project_champitheque/Application.java
@@ -0,0 +1,27 @@
+package com.example.project_champitheque;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    private Stage primaryStage;
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Champithèque");
+        stage.getIcons().add(new Image(Application.class.getResourceAsStream("/img/logo.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/project_champitheque/ScreenController.java
@@ -0,0 +1,29 @@
+package com.example.project_champitheque;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class ScreenController {
+
+    @FXML
+    private Text Pseudo;
+
+    @FXML
+    private Text level;
+
+    @FXML
+    private Button Game_1;//boutton pour lancer jeu
+
+    public void handleGame1() throws IOException {//fonction pour changer de jeu, ici du menu de base au menu moche de corentin
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("champitheque.fxml"));
+        Stage window =(Stage) Game_1.getScene().getWindow();
+        window.setScene(new Scene(fxmlLoader.load()));
+    }
+}
