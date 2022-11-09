99026c037456259c42f5f3cd39a7cf7337b57105
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2022-11-09 18:20:21 +0100

    Marche pas

--- src/main/java/com/example/project_champitheque/MenuModel.java
+++ src/main/java/com/example/project_champitheque/MenuModel.java
@@ -5,11 +5,32 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner;
+
 public class MenuModel {
     private final IntegerProperty level;
     private final StringProperty pseudo;
 
+    /*public MenuModel(Integer ranking, String name) {
+        this.level = new SimpleIntegerProperty(ranking);
+        this.pseudo = new SimpleStringProperty(name);
+    }*/
+
     public MenuModel(Integer ranking, String name) {
+        try {
+            File myObj = new File("../../resources/data/players.txt");
+            Scanner myReader = new Scanner(myObj);
+            /*while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+            }*/
+            System.out.println(myReader);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         this.level = new SimpleIntegerProperty(ranking);
         this.pseudo = new SimpleStringProperty(name);
     }
