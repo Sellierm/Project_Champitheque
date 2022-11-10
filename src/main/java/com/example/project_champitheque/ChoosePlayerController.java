a8d9faab722f477f869365c131e211a8b8692d18
Corentin-Bocquet <corentin.bocquet@student.yncrea.fr>
2022-11-10 15:07:43 +0100

    ChoosePlayer

--- src/main/java/com/example/project_champitheque/ChoosePlayerController.java
+++ src/main/java/com/example/project_champitheque/ChoosePlayerController.java
@@ -1,4 +1,17 @@
 package com.example.project_champitheque;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class ChoosePlayerController {
+
+    ChoosePlayerModel tmp;
+
+    @FXML
+    private Text newName;   // Zone de texte où est inscrit le nouveau pseudo
+
+    public void initialize(){
+        // IL NOUS FAUT MAINTENANT ECRIRE UNE FONCTION QUI ECRIT LE NOUVEAU PSEUDO DANS UN FICHIER TEXTE
+        //   + UN CHANGEMENT DE SCENE
+    }
 }
--- src/main/java/com/example/project_champitheque/ChoosePlayerModel.java
+++ src/main/java/com/example/project_champitheque/ChoosePlayerModel.java
@@ -1,4 +1,26 @@
 package com.example.project_champitheque;
 
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ChoosePlayerModel {
+
+    // VARIABLE
+    private final StringProperty newName;
+
+    // CONSTRUCTEUR
+    public ChoosePlayerModel(String newName) {
+        this.newName = new SimpleStringProperty(newName);
+    }
+
+    // GETTERS
+
+    public String getNewName() {
+        return newName.get();
+    }
+
+    public StringProperty newNameProperty() {
+        return newName;
+    }
 }
